637a34394f640b001d4b077270f9386053737fe8
hyuntae <bht9011@gmail.com>
2024-05-25 23:28:50 +0900

    fix router cors error

--- app/external/router.go
+++ app/external/router.go
@@ -1,14 +1,23 @@
 package external
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"poten-invitation-golang/domain"
+	"time"
 )
 
 func GetRouter(expenseController domain.ExpenseController) *gin.Engine {
 	gin.ForceConsoleColor()
 	r := gin.Default()
+	r.Use(cors.New(
+		cors.Config{
+			AllowOrigins: []string{"https://www.gardenr.kr/"},
+			AllowMethods: []string{http.MethodPost, http.MethodPut, http.MethodDelete},
+			MaxAge:       12 * time.Hour,
+		},
+	))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
