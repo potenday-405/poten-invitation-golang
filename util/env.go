cf8b926d1ebc99bba24f1adc946aa483bf1c2a95
hyuntae <bht9011@gmail.com>
2024-06-01 14:14:59 +0900

    fix env directory

--- util/env.go
+++ util/env.go
@@ -3,7 +3,7 @@ package util
 import "github.com/joho/godotenv"
 
 func EnvInitializer() error {
-	if err := godotenv.Load("./env/.env"); err != nil {
+	if err := godotenv.Load("app/env/.env"); err != nil {
 		return err
 	}
 	return nil
